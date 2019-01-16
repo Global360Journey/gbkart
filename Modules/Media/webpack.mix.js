16f4c290e8541bc9ae6b08be2f01d60e7d092945
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-01-16 20:57:38 +0530

    Deleted Old Project

--- Modules/Media/webpack.mix.js
+++
@@ -1,4 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('Modules/Media/Resources/assets/admin/js/main.js', 'Modules/Media/Assets/admin/js/media.js')
-    .sass('Modules/Media/Resources/assets/admin/sass/main.scss', 'Modules/Media/Assets/admin/css/media.css');
--- Modules/Report/webpack.mix.js
+++
@@ -1,4 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('Modules/Report/Resources/assets/admin/js/main.js', 'Modules/Report/Assets/admin/js/report.js')
-    .sass('Modules/Report/Resources/assets/admin/scss/main.scss', 'Modules/Report/Assets/admin/css/report.css');
