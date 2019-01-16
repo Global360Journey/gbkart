70129b01261a0de5146248a667100c547a263b8f
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-01-16 16:16:56 +0530

    Added new code

--- Modules/Option/webpack.mix.js
+++
@@ -1,4 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('Modules/Option/Resources/assets/admin/js/main.js', 'Modules/Option/Assets/admin/js/option.js')
-    .sass('Modules/Option/Resources/assets/admin/sass/main.scss', 'Modules/Option/Assets/admin/css/option.css');
