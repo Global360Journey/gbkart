11ec39232824cbf8f1daaba3f4c24ddc9a59d0c7
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-01-18 01:29:12 +0530

    remove

--- Modules/Attribute/webpack.mix.js
+++
@@ -1,4 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('Modules/Attribute/Resources/assets/admin/js/main.js', 'Modules/Attribute/Assets/admin/js/attribute.js')
-    .sass('Modules/Attribute/Resources/assets/admin/sass/main.scss', 'Modules/Attribute/Assets/admin/css/attribute.css');
