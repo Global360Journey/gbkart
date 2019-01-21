55516250e91cdd149991015ec1253a5962aeff7e
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-01-21 10:08:29 +0530

    Remove Footer

--- Modules/Admin/Resources/views/partials/footer.blade.php
+++ Modules/Admin/Resources/views/partials/footer.blade.php
@@ -27,5 +27,5 @@
         <i class="fa fa-keyboard-o"></i>
     </a>&nbsp;
 
-    <span>Copyright &copy; {{ date('Y')}} <a href="http://envaysoft.com/fleetcart" target="_blank">FleetCart</a></span>
+    <span>Copyright &copy; {{ date('Y')}} <a href="http://global360journey.com/" target="_blank">Global360journey</a></span>
 </footer>
