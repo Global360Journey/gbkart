448fd16c4a409402310cd0b7a9a72b95cf1d4f6d
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-02-18 11:43:34 +0530

    Added New Code

--- Themes/Storefront/views/public/home/sections/partials/slider_banner.blade.php
+++
@@ -1,8 +0,0 @@
-<a href="{{ $sliderBanner->url }}" class="banner" style="background-image: url({{ $sliderBanner->image->path }});">
-    <div class="overlay"></div>
-
-    <div class="banner-content">
-        <h3>{{ $sliderBanner->caption_1 }}</h3>
-        <p>{{ $sliderBanner->caption_2 }}</p>
-    </div>
-</a>
